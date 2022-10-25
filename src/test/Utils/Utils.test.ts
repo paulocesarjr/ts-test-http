b9da18f8e2365df8904e97f4e0f7f6183a2d0d2d
Paulo Cesar Jr <paulocesarrosajr@gmail.com>
2022-10-25 13:51:11 -0300

    test(utils): check valid request

---
+++ src/test/Utils/Utils.test.ts
@@ -0,0 +1,23 @@
+/** @format */
+import { IncomingMessage } from "http"
+import { Utils } from "../../app/Utils/Utils"
+
+describe("Utils test suit", () => {
+  test("getRequestPath valid request", () => {
+    const request = {
+      url: "http://localhost:8080/login"
+    } as IncomingMessage
+
+    const resultPath = Utils.getRequestBasePath(request)
+    expect(resultPath).toBe("login")
+  })
+
+  test("getRequestPath with no path name", () => {
+    const request = {
+      url: "http://localhost:8080/"
+    } as IncomingMessage
+
+    const resultPath = Utils.getRequestBasePath(request)
+    expect(resultPath).toBeFalsy()
+  })
+})
