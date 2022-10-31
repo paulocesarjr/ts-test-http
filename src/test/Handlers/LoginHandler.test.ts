80c2c534669bfd0861b56ee839b2ea29e2bb4c5f
Paulo Cesar Jr <paulocesarrosajr@gmail.com>
2022-10-31 13:56:07 -0300

    test(LoginHandler): test login request

--- src/test/Handlers/LoginHandler.test.ts
+++ src/test/Handlers/LoginHandler.test.ts
@@ -1,7 +1,12 @@
 /** @format */
 
 import { LoginHandler } from "../../app/Handlers/LoginHandler"
-import { HTTP_CODES, HTTP_METHODS } from "../../app/Models/ServerModels"
+import {
+  HTTP_CODES,
+  HTTP_METHODS,
+  SessionToken
+} from "../../app/Models/ServerModels"
+import { Utils } from "../../app/Utils/Utils"
 
 describe("LoginHandler test suite", () => {
   let loginHandler: LoginHandler
@@ -11,9 +16,15 @@ describe("LoginHandler test suite", () => {
   }
 
   const responseMock = {
-    writeHead: jest.fn() // cria uma função simulada
+    writeHead: jest.fn(), // cria uma função simulada
+    write: jest.fn()
   }
-  const authorizerMock = {}
+
+  const authorizerMock = {
+    generateToken: jest.fn()
+  }
+
+  const getRequestBody = jest.fn()
 
   // executa antes de cada caso de teste
   beforeEach(() => {
@@ -22,12 +33,22 @@ describe("LoginHandler test suite", () => {
       responseMock as any,
       authorizerMock as any
     )
+
+    Utils.getRequestBody = getRequestBody
   })
 
   // executa após cada caso de teste
   afterEach(() => {
     jest.clearAllMocks() // limpa todos as simulações
   })
+
+  const someSessionToken: SessionToken = {
+    tokenId: "someTokenId",
+    userName: "tainhaplay",
+    valid: true,
+    expirationTime: new Date(),
+    accessRights: [1, 2, 3]
+  }
 
   test("options request", async () => {
     requestMock.method = HTTP_METHODS.OPTIONS
@@ -39,5 +60,19 @@ describe("LoginHandler test suite", () => {
     requestMock.method = "algumMetodoAleatorio"
     await loginHandler.handleRequest()
     expect(responseMock.writeHead).not.toHaveBeenCalled()
+  })
+
+  test.only("post request with valid login", async () => {
+    requestMock.method = HTTP_METHODS.POST
+
+    getRequestBody.mockReturnValueOnce({
+      username: "tainhaplay",
+      passwrod: "abc@123"
+    })
+
+    authorizerMock.generateToken.mockReturnValueOnce(someSessionToken) // retorna um valor mocado quando a função for chamada
+    await loginHandler.handleRequest()
+
+    // mockReturnValueOnce permite encadeamento de chamadas
   })
 })
